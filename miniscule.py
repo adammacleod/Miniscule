5b002fef0805d9f19331dd4435e3aae7e48edd08
Adam MacLeod <adam.macleod@gmail.com>
2011-08-23 20:13:52 +1000

    Refactored some of the procedures in the main code block into their own functions.
    Commented everything up.

--- miniscule.py
+++ miniscule.py
@@ -1,7 +1,46 @@
 import os, sys, shutil, markdown
 from jinja2 import Environment, FileSystemLoader
 
+"""Miniscule is a static website generator that makes YOU do all of the work.
+
+It is designed to be as simple as possible.
+
+This project is available at http://www.github.com/adammacleod/Miniscule
+"""
+
+def init_constants():
+    """Initializes the location of directories used through the remainder of
+    the project.
+    """
+    global PROJECT_DIR, WEBSITE_DIR, OUTPUT_DIR, TEMPLATE_DIR, CSS_DIR
+    
+    PROJECT_DIR = '.'
+    if len(sys.argv) > 1:
+        PROJECT_DIR = os.path.normpath(sys.argv[1])
+
+    WEBSITE_DIR = os.path.abspath(os.path.join(PROJECT_DIR, 'website'))
+    OUTPUT_DIR = os.path.join(WEBSITE_DIR, '../output')
+    TEMPLATE_DIR = os.path.join(WEBSITE_DIR, '../templates')
+    CSS_DIR = os.path.join(TEMPLATE_DIR, 'css')
+
+def init_libs():
+    """Initializes the Jinja and Markdown libraries."""
+    global JINJA_ENV, MARKDOWN
+    
+    JINJA_ENV = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+
+    MARKDOWN = markdown.Markdown(
+        extensions=['meta'], 
+        output_format='html4'
+    )
+    
 def process_dir(dir):
+    """Recursively searches the website directory for files to process.
+    
+    Keyword arguments:
+    dir -- The first call will be the website directory. 
+           Subsequent calls will be the children of previous calls.
+    """
     for item in os.listdir(dir):
         full_path = os.path.join(dir, item)
         
@@ -11,6 +50,12 @@ def process_dir(dir):
             process_dir(full_path)
             
 def process_file(dir, file):
+    """Reads file from dir, renders it and then writes to the output directory.
+    
+    Keyword arguments:
+    dir -- Directory where file exists.
+    file -- Full filename of the file (eg. index.md)
+    """
     [filename, file_ext] = os.path.splitext(file)
     if file_ext != '.md':
         return
@@ -23,20 +68,37 @@ def process_file(dir, file):
     write_file(dir, filename, output)
 
 def render(page):
-    html = md.convert(page)
+    """Renders markdown source into a html format.
+    The source is first run through the markdown parser, and then fed into the
+    Jinja2 template engine.
     
-    template = env.get_template(md.Meta["template"][0] + ".html")
-    output = template.render(content = html, meta = md.Meta)
+    Keyword arguments:
+    page -- A string containing the markdown source.
+    """
+    html = MARKDOWN.convert(page)
+    
+    template = JINJA_ENV.get_template(MARKDOWN.Meta["template"][0] + ".html")
+    output = template.render(content = html, meta = MARKDOWN.Meta)
 
-    md.reset() #Clears meta data
+    MARKDOWN.reset() # Clears meta data
     
     return output
 
 def write_file(source_dir, filename, contents):
-    output_path = os.path.normpath(
-                               os.path.join(
-                                        output_dir,
-                                        os.path.relpath(source_dir, website_dir)))
+    """Writes contents into filename.
+    
+    Keyword arguments:
+    source_dir -- The source directory of the original file. This directory
+                  will have the website source directory stripped and the
+                  output directory prepended.
+    filename -- The name of the file to write into. .html is appended to this
+                value.
+    contents -- The contents of the file to write.
+    """
+    # rel_path is the position of the file relative to the source website root
+    #   This is needed to determine where to write the output file.
+    rel_path = os.path.relpath(source_dir, WEBSITE_DIR)
+    output_path = os.path.join(OUTPUT_DIR, rel_path)
                                         
     output_file = os.path.join(output_path, filename + '.html')
     
@@ -46,29 +108,20 @@ def write_file(source_dir, filename, contents):
     with open(output_file, 'w') as f:
         f.write(contents)            
 
-def copy_css_to_output():
-    if os.path.exists(css_dir):
-        shutil.copytree(css_dir, os.path.join(output_dir, 'css'))
+def copy_css():
+    """Searches for CSS_DIR and copies it into the OUTPUT_DIR/css if it exists.
+    """
+    if os.path.exists(CSS_DIR):
+        shutil.copytree(CSS_DIR, os.path.join(OUTPUT_DIR, 'css'))
 
 if __name__ == '__main__':
-    project_dir = '.'    
-    if len(sys.argv) > 1:
-        project_dir = os.path.normpath(sys.argv[1])
+    init_constants()
     
-    website_dir = os.path.abspath(os.path.join(project_dir, 'website'))
-    output_dir = os.path.join(website_dir, '../output')
-    template_dir = os.path.join(website_dir, '../templates')
-    css_dir = os.path.join(template_dir, 'css')
+    init_libs()
     
-    shutil.rmtree(output_dir, True)
+    # Clear the output directory.
+    shutil.rmtree(OUTPUT_DIR, True)
+     
+    process_dir(WEBSITE_DIR)
     
-    env = Environment(loader=FileSystemLoader(template_dir))
-
-    md = markdown.Markdown(
-        extensions=['meta'], 
-        output_format='html4'
-    )
-    
-    process_dir(website_dir)
-    
-    copy_css_to_output()
+    copy_css()
